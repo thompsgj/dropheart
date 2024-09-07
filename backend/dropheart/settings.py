768a632f5533de1ae7f62ad8909355d6188ef095
Greg Thompson <thompsgj@yahoo.com>
2024-09-08 03:40:07 +0900

    fix: turn off hard-coded DEBUG

--- backend/dropheart/settings.py
+++ backend/dropheart/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3y!k=4g)+vwc9lre-hd!ar9)*@*3ajk3&q2f9rsc#gc03)%hd7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = (
     os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else []
