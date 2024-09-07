fe946365f268de542d33479844f36dc731d2fa66
zEleceed <133176949+zEleceed@users.noreply.github.com>
2024-09-07 22:48:43 +0900

    Update models.py
    annulled initial class Item

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,22 +3,22 @@ import uuid
 from django.db import models
 
 
-class Item(models.Model):
-    ITEM_CHOICES = (("SOCKS", "socks"), ("PADS", "pads"), ("UNDERWEAR", "underwear"))
-    item_id = models.UUIDField(
-        default=uuid.uuid4,
-        unique=True,
-        help_text="A unique ID associated with the item",
-    )
-    location_id = models.TextField(help_text="")
-    item_type = models.CharField(
-        choices=ITEM_CHOICES, help_text="Type of item dropped off", max_length=15
-    )
-    image_path = models.TextField(help_text="Path the image location in cloud bucket")
-    # user_id = models.ForeignKey(User, help_text="The user id who dropped off the item")
-    list_time = models.DateTimeField(
-        auto_now=True, help_text="The time that the item was listed"
-    )
+# class Item(models.Model):
+#     ITEM_CHOICES = (("SOCKS", "socks"), ("PADS", "pads"), ("UNDERWEAR", "underwear"))
+#     item_id = models.UUIDField(
+#         default=uuid.uuid4,
+#         unique=True,
+#         help_text="A unique ID associated with the item",
+#     )
+#     location_id = models.TextField(help_text="")
+#     item_type = models.CharField(
+#         choices=ITEM_CHOICES, help_text="Type of item dropped off", max_length=15
+#     )
+#     image_path = models.TextField(help_text="Path the image location in cloud bucket")
+#     # user_id = models.ForeignKey(User, help_text="The user id who dropped off the item")
+#     list_time = models.DateTimeField(
+#         auto_now=True, help_text="The time that the item was listed"
+#     )
 
 
 from django.db import models
