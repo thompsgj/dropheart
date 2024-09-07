8661c286e34e5fcc355c9b1ccf2cc10bb3fbc54b
zEleceed <133176949+zEleceed@users.noreply.github.com>
2024-09-08 05:22:19 +0900

    feat: login and registration

--- backend/dropheart/urls.py
+++ backend/dropheart/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from auth.views import LogoutView
 
 urlpatterns = [
+    path('auth/', include('auth.urls')),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("auth/logout/", LogoutView.as_view()),
